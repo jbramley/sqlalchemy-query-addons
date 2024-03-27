feea9a51ecd5206ccc1afe00cd34e4a20746cda7
Justin Bramley <jbramley@gmail.com>
2024-03-27 08:24:22 -0400

    Make less squigglies

--- lib/sqlalchemy_query_addons/__init__.py
+++ lib/sqlalchemy_query_addons/__init__.py
@@ -3,8 +3,8 @@ from typing import Any
 from sqlalchemy import Select
 from sqlalchemy.sql._typing import _ColumnsClauseArgument
 
-from sqlalchemy_query_addons.smartjoins._select import SmartJoinsSelect
-from sqlalchemy_query_addons.smartjoins._compile_state import (
+from sqlalchemy_query_addons.smartjoins import (
+    SmartJoinsSelect,
     SmartJoinsORMSelectCompileState as SmartJoinsORMSelectCompileState,
 )
 
---
+++ lib/sqlalchemy_query_addons/smartjoins/__init__.py
@@ -0,0 +1,10 @@
+from sqlalchemy_query_addons.smartjoins._compile_state import (
+    SmartJoinsORMSelectCompileState,
+)
+from sqlalchemy_query_addons.smartjoins._select import SmartJoinsSelect
+
+
+__all__ = [
+    "SmartJoinsSelect",
+    "SmartJoinsORMSelectCompileState",
+]
